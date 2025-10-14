5952db4bf46e8a94102d6d6d3537e946d692f355
Joao Felipe Gobeti <joao.gobeti@motora.ai>
2025-10-14 13:00:26 -0300

    fixing extRPY to its inverse

--- src/imuPreintegration.cpp
+++ src/imuPreintegration.cpp
@@ -204,8 +204,8 @@ public:
     // gtsam::Pose3 imu2Lidar = gtsam::Pose3(gtsam::Rot3(extRot.transpose()), gtsam::Point3(-extTrans.x(), -extTrans.y(), -extTrans.z()));
     
     // T_lb: tramsform points from imu frame to lidar frame
-    // gtsam::Pose3 lidar2Imu = gtsam::Pose3(gtsam::Rot3(1, 0, 0, 0), gtsam::Point3(extTrans.x(), extTrans.y(), extTrans.z()));
-    gtsam::Pose3 lidar2Imu = gtsam::Pose3(gtsam::Rot3(extRot), gtsam::Point3(extTrans.x(), extTrans.y(), extTrans.z()));
+    gtsam::Pose3 lidar2Imu = gtsam::Pose3(gtsam::Rot3(1, 0, 0, 0), gtsam::Point3(extTrans.x(), extTrans.y(), extTrans.z()));
+    // gtsam::Pose3 lidar2Imu = gtsam::Pose3(gtsam::Rot3(extRot), gtsam::Point3(extTrans.x(), extTrans.y(), extTrans.z()));
 
 
     IMUPreintegration()
